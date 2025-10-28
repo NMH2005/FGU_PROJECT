1c4de8aaef665786b1f8d1b49133f952ac0afee7
nmh14022005 <158242591+nmh14022005@users.noreply.github.com>
2025-10-29 01:04:46 +0700

    WIP

---
+++ Assets/Script/BubbleFollow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class BubbleFollow : MonoBehaviour {
+    public Transform target; // nhân vật đang nói
+    public Vector3 offset;   // khoảng cách so với nhân vật
+
+    void LateUpdate()
+    {
+        if (target != null)
+            transform.position = target.position + offset;
+    }
+}
---
+++ Assets/Script/DialogueController.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class DialogueController : MonoBehaviour {
+    public TextMeshProUGUI dialogueText; // chỉ trỏ tới TextMeshProUGUI bên trong BubbleChat
+    public GameObject bubbleChat;        // trỏ tới BubbleChat (Image + Tail)
+
+    public void ShowDialogue(string text)
+    {
+        dialogueText.text = text;
+        bubbleChat.SetActive(true);  // bật toàn bộ bubble (Image + Tail + Text)
+    }
+
+    public void HideDialogue()
+    {
+        bubbleChat.SetActive(false);
+    }
+}
