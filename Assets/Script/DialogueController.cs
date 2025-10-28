98a26942ff826f40591661fd6ce3d9b1f0d6efea
nmh14022005 <158242591+nmh14022005@users.noreply.github.com>
2025-10-29 02:20:58 +0700

    done scene 7

--- Assets/Script/DialogueController.cs
+++ Assets/Script/DialogueController.cs
@@ -1,18 +1,44 @@
-﻿using TMPro;
-using UnityEngine;
+﻿using UnityEngine;
+using TMPro;
 
 public class DialogueController : MonoBehaviour {
-    public TextMeshProUGUI dialogueText; // chỉ trỏ tới TextMeshProUGUI bên trong BubbleChat
-    public GameObject bubbleChat;        // trỏ tới BubbleChat (Image + Tail)
+    public TextMeshProUGUI dialogueText;
+    public BubbleFollow bubbleFollow;
+    public Transform main;
+    public Transform wifeSoul;
+
+    private Transform currentSpeaker;
+
+    public void SetSpeakerToMain()
+    {
+        currentSpeaker = main;
+    }
+
+    public void SetSpeakerToWife()
+    {
+        currentSpeaker = wifeSoul;
+    }
 
     public void ShowDialogue(string text)
     {
+        if (dialogueText == null || bubbleFollow == null)
+        {
+            Debug.LogWarning("DialogueText hoặc BubbleFollow chưa được gán!");
+            return;
+        }
+
         dialogueText.text = text;
-        bubbleChat.SetActive(true);  // bật toàn bộ bubble (Image + Tail + Text)
+        dialogueText.gameObject.transform.parent.gameObject.SetActive(true); // bật cả BubbleChat
+        dialogueText.gameObject.SetActive(true);
+
+        if (currentSpeaker != null)
+            bubbleFollow.SetTarget(currentSpeaker);
+        else
+            Debug.LogWarning("Chưa gọi SetSpeakerToMain/Wife trước khi ShowDialogue!");
     }
 
     public void HideDialogue()
     {
-        bubbleChat.SetActive(false);
+        dialogueText.gameObject.transform.parent.gameObject.SetActive(false);
     }
 }
